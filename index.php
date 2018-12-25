f03ab4d7bd412415eef5b1e5d91d13545f819b19
Antonina <tone4kawow@gmail.com>
2018-12-25 22:37:21 +0300

    If-else-switch in php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+	define("DBNAME", "kinomonster");
+
 	$nickname = "Pukki";
 
 	$balance = 0;
@@ -8,6 +10,30 @@
 
 	$balance = $addBalance1 + $addBalance2;
 
+	$svetofor = "";
+	/*if($svetofor == "green") {
+		echo "Светофор зеленый. Я перехожу дорогу";
+	} else if($svetofor == "yellow") {
+		echo "Сейчас светофор желтый";
+	}
+	else {
+		echo "Жду, когда загорится зеленый";
+	}*/
+
+	switch ($svetofor) {
+		case 'green':
+	echo "Светофор зеленый. Я перехожу дорогу";
+			break;
+		case 'yellow':
+	echo "Сейчас светофор желтый";
+			break;
+		case 'red':
+	echo "Жду, когда загорится зеленый";
+			break;
+		default:
+	echo "Светофор сломался, пойду искать другой пешеходный переход";
+			break;
+	}
 ?>
 
 <!DOCTYPE html>
@@ -17,9 +43,10 @@
 	<title>Document</title>
 </head>
 <body> 
-	<div>Hello, <?php echo $nickname; ?></div>
+	<div><!--Hello,--> <?php //echo $nickname; ?></div>
 	<?php
-		echo "<div>".$balance."</div>";
+		/*echo "<div>".$balance."</div>";
+		echo DBNAME;*/
 	?>
 </body>
 </html>
