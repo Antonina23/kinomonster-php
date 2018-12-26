f58ab184b0dcf0cc5edd8f959c6f1702ae153a97
Antonina <tone4kawow@gmail.com>
2018-12-26 22:50:24 +0300

    Multi array

--- index.php
+++ index.php
@@ -1,35 +1,38 @@
 <?php
-// добавление в массив:
-$arr = ["продукты", "бутылка воды"];
 
-//unset($arr[0]);
+/*$camera["row1"][0] = "продукты";
+$camera["row1"][1] = "сумка";
+$camera["row2"][0] = "апельсин";
+$camera["row2"][1] = "фотоаппарат";
+$camera["row2"][2] = "телефон";
 
-array_push($arr, "яблоко");
-
-//добавление в начало:
-array_unshift($arr, "апельсин");
-array_push($arr, "апельсин");
-
-$arr = array_unique($arr);
-
-$arr2 = array("кокос", "арбуз", "банан");
-
-$newarr = array_merge($arr, $arr2);
-
-foreach ($newarr as $key => $value) {
-	echo "Ключ к массиву: ".$key." - Значение массива: ".$value."<br>";
-}
-
-// преобразует массив в строку:
-/*$str = implode(", ", $newarr);
-echo "<br>".$str;*/
-
-// преобразует строку в массив:
-$str = "продукты, бутылка воды, кокос, банан";
-$strtoarr = explode(", ", $str);
 echo "<pre>";
-print_r($strtoarr);
+print_r($camera);
 echo "</pre>";
+
+foreach ($camera as $key => $value) {
+	foreach ($value as $key2 => $value2) {
+		echo $value2;
+	}
+}*/
+
+$camera["market1"]["row1"][0] = "продукты";
+$camera["market1"]["row1"][1] = "сумка";
+$camera["market1"]["row2"][0] = "апельсин";
+$camera["market2"]["row2"][1] = "фотоаппарат";
+$camera["market2"]["row2"][2] = "телефон";
+
+echo "<pre>";
+print_r($camera);
+echo "</pre>";
+
+foreach ($camera as $key => $value) {
+	foreach ($value as $key2 => $value2) {
+		foreach ($value2 as $key3 => $value3) {
+			echo $value3;
+		}
+	}
+}
 ?>
 
 <!DOCTYPE html>
