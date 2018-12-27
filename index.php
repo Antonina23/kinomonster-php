12f285d60ed7bcd1d49928beb10d003abbf5df9b
Antonina <tone4kawow@gmail.com>
2018-12-27 21:48:44 +0300

    INSERT mysql

--- index.php
+++ index.php
@@ -9,11 +9,14 @@ if(mysqli_connect_error()) {
 
 $mysqli->set_charset('utf8');
 
-$query = $mysqli->query('SELECT * FROM movie');
+/*$query = $mysqli->query('SELECT * FROM movie');
 
 while ($row = mysqli_fetch_assoc($query)) {
 	echo $row['name'].$row['year']."<br>";
-}
+}*/
+
+$query = "INSERT INTO movie VALUES(null, 'Фантастические твари и где они обитают - преступления Грин-де-Вальда', 'Вторая часть невероятно захватывающей сказки', '2018', Now())";
+$mysqli->query($query);
 
 $mysqli->close();
 
