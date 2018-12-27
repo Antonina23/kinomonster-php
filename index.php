b5be3d9ac72022aa10b65925a52ced9228782c12
Antonina <tone4kawow@gmail.com>
2018-12-27 23:26:31 +0300

    XML for mysql in php

--- index.php
+++ index.php
@@ -1,18 +1,51 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'root', '', 'kinomonster');
+function insert($name, $desk, $year, $rating, $poster, $category_id) {
+	$mysqli = new mysqli('localhost', 'root', '', 'kinomonster');
+	if(mysqli_connect_errno()) {
+		printf('Соединение не установлено');
+		exit();
+	}
 
-if(mysqli_connect_error()) {
-	printf("Соединение не установлено", mysqli_connect_error());
-	exit();
+	$mysqli->set_charset('utf8');
+	$query = "INSERT INTO movie VALUES(null, '$name', '$desk', '$year', '$rating', '$poster', Now(), '$category_id')";
+
+	$result = false;
+	if($mysqli->query($query)) {
+		$result = true;
+	}
+	return $result;
 }
 
-$mysqli->set_charset('utf8');
+$xml = simplexml_load_file("xml/movies.xml") or die("Error: Cannot create an object");
 
-$query = "DELETE FROM movie WHERE id = 3";
-$mysqli->query($query);
+//echo count($xml);
 
-$mysqli->close();
+$title = null;
+$title_orign = null;
+$post = null;
+$rating = null;
+$year = null;
+
+foreach ($xml as $movie_key => $movie) {
+	$title = $movie->title_russian."<br>";
+	$title_orign = $movie->title_original;
+	$year = $movie->year;
+
+	foreach ($movie->poster->big->attributes() as $poster_key => $poster) {
+		$post = $poster."<br>";
+	}
+	if($movie->imdb) {
+		$rating = $movie->imdb->attributes()['rating'];
+	} else {
+		$rating = null;
+	}
+	insert($title, $title_orign, $year, $rating, $post, 1);
+}
+
+echo "<pre>";
+print_r($xml);
+echo "</pre>";
 
 ?>
 
